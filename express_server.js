934fda25a35c0d0669a9130875bb235ebb0a9455
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-10 03:36:32 +0000

    Added Logic to handle POST method for form submition

--- express_server.js
+++ express_server.js
@@ -44,3 +44,8 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
